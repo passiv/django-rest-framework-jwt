83ae24a0ebec5054db827a72db44ae2530e65d58
Nick McCullum <nicholasmccullum@gmail.com>
2022-03-16 19:31:39 -0400

    Update PyJWT version to accept <3.0.0

--- setup.py
+++ setup.py
@@ -13,21 +13,21 @@ def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-    with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
-        src = init_py.read().decode('utf-8')
+    with open(os.path.join(package, "__init__.py"), "rb") as init_py:
+        src = init_py.read().decode("utf-8")
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
 
 
-name = 'djangorestframework-jwt'
-version = get_version('rest_framework_jwt')
-package = 'rest_framework_jwt'
-description = 'JSON Web Token based authentication for Django REST framework'
-url = 'https://github.com/GetBlimp/django-rest-framework-jwt'
-author = 'Jose Padilla'
-author_email = 'jpadilla@getblimp.com'
-license = 'MIT'
+name = "djangorestframework-jwt"
+version = get_version("rest_framework_jwt")
+package = "rest_framework_jwt"
+description = "JSON Web Token based authentication for Django REST framework"
+url = "https://github.com/GetBlimp/django-rest-framework-jwt"
+author = "Jose Padilla"
+author_email = "jpadilla@getblimp.com"
+license = "MIT"
 install_requires = [
-    'PyJWT>=1.5.2,<2.0.0',
+    "PyJWT>=1.5.2,<3.0.0",
 ]
 
 
@@ -35,7 +35,7 @@ def read(*paths):
     """
     Build a file path from paths and return the contents.
     """
-    with open(os.path.join(*paths), 'r') as f:
+    with open(os.path.join(*paths), "r") as f:
         return f.read()
 
 
@@ -43,9 +43,11 @@ def get_packages(package):
     """
     Return root package and all sub-packages.
     """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
 
 
 def get_package_data(package):
@@ -53,29 +55,30 @@ def get_package_data(package):
     Return all files under the root package, that are not in a
     package themselves.
     """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    walk = [
+        (dirpath.replace(package + os.sep, "", 1), filenames)
+        for dirpath, dirnames, filenames in os.walk(package)
+        if not os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
 
     filepaths = []
     for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
+        filepaths.extend([os.path.join(base, filename) for filename in filenames])
     return {package: filepaths}
 
 
-if sys.argv[-1] == 'publish':
-    if os.system('pip freeze | grep twine'):
-        print('twine not installed.\nUse `pip install twine`.\nExiting.')
+if sys.argv[-1] == "publish":
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
         sys.exit()
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    shutil.rmtree('dist')
-    shutil.rmtree('build')
-    shutil.rmtree('djangorestframework_jwt.egg-info')
-    print('You probably want to also tag the version now:')
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    shutil.rmtree("dist")
+    shutil.rmtree("build")
+    shutil.rmtree("djangorestframework_jwt.egg-info")
+    print("You probably want to also tag the version now:")
     print("  git tag -a {0} -m 'version {0}'".format(version))
-    print('  git push --tags')
+    print("  git push --tags")
     sys.exit()
 
 
@@ -85,27 +88,27 @@ setup(
     url=url,
     license=license,
     description=description,
-    long_description=read('README.rst'),
+    long_description=read("README.rst"),
     author=author,
     author_email=author_email,
     packages=get_packages(package),
     package_data=get_package_data(package),
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
